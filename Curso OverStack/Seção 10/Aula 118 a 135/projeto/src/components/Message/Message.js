216c8233a0b56b44eb726e252d99b43e7adc259f
eder0782 <eder0782@gmail.com>
2022-04-27 22:02:40 -0400

    melhorias na tela de autenticação

---
+++ Curso OverStack/Seção 10/Aula 118 a 135/projeto/src/components/Message/Message.js
@@ -0,0 +1,29 @@
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Message(message,type){
+
+    switch (type) {
+        case 'error':
+            toast.error(message);
+            break;
+        case 'sucess':
+            toast.success(message);
+            break;
+        case 'info':
+            toast.info(message);
+            break;
+        case 'warn':
+            toast.warn(message);
+            break;
+
+        default:
+            toast(message);
+            break;
+    }
+
+
+}
+
+
+export default Message;
--- Curso OverStack/Seção 10/Aula 118 a 135/projeto/src/pages/signIn/SignIn.js
+++ Curso OverStack/Seção 10/Aula 118 a 135/projeto/src/pages/signIn/SignIn.js
@@ -1,31 +1,49 @@
 import { useState } from 'react';
 import api from '../../services/api';
-import axios from "axios";
+// import axios from "axios";
+import { ToastContainer } from 'react-toastify';
 
+import Message from '../../components/Message/Message';
+import SigninValidation from '../../utils/validation/SigninValidation';
 import {Container,Form,Input,Button} from './styleSignIn'
 
 function SignIn(){
     const [email,setEmail]=useState('');
     const [password,setPassword]=useState('');
     //FUNÇÃO ASSINCRONA UTILIZANDO AXIOS
-    async function HandleClick(){
+    async function HandleClick(e){
+        e.preventDefault();
        const data = {email,password}
 
-       await axios.post('http://localhost:3000/user',data)
-       .then((response)=>{
-           console.log(response)
-       })
-       .catch((error)=>{
-           console.error(error);
-       })
+        //VERIFICA SE O EMAIL E A SENHA PREENCHIDOS SÃO VÁLIDOS
+        //PARA ISSO UTILIZA A BIBLIOTECA YUP
+       let validation = await SigninValidation(data);
+
+       if(validation){
+            await api.post('http://localhost:3000/user',data)
+            .then((response)=>{
+                Message(response)
+            })
+            .catch((error)=>{
+                Message('Falha na autenticação','error');
+            })
+
+       }
+       else{
+           Message('Preencha um email e uma senha válidos!','warn');
+
+       }
+
+       
 
     }
     return(
         <Container>
+            <ToastContainer theme='colored'/>
             <Form>
                 <Input type='email' required placeholder='Email' onChange={(e)=>setEmail(e.target.value)}></Input>
-                <Input type='password' required placeholder='Senha' onChange={(e)=>setPassword(e.target.value)}></Input>
-                <Button onClick={HandleClick}>Entrar</Button>
+                <Input type='password'  placeholder='Senha' onChange={(e)=>setPassword(e.target.value)}></Input>
+                <Button onClick={(e)=>HandleClick(e)}>Entrar</Button>
 
             </Form>
             
